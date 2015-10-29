67a592dd3863be4d7fa596927894a535daf7be7b
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-10-29 12:34:45 -0700

    add js dependencies

---
+++ assets/js/dependencies/custom.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+    
+    setTimeout(function sunrise () {
+          document.getElementsByClassName('header')[0].style.backgroundColor = '#118798';
+    }, 0);  
+    
+    $('#searchResults').DataTable();
+
+
+} );
+
